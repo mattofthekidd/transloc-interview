f4964b4cce298c6cb5fcb5457c80fdee01a6dc1a
Matthew Kidd <mattofthekidd@gmail.com>
2021-01-27 22:50:32 -0700

    cleaning up a bit
    I'm doing it Peter!

--- index.js
+++ index.js
@@ -2,63 +2,81 @@
 const express = require('express');
 const path = require('path')
 var app = express();
+const request = require('request');
 
 const fs = require('fs');
 const parse = require('csv-parser');
-const results = [
-    {
-        "type": "FeatureCollection",
-        "features": [],
-    }
-];
+const results = [];
 
-console.log(__dirname)
-console.log(path.join(__dirname))
+// console.log(__dirname)
+// console.log(path.join(__dirname))
 
 const file = "./public/data/GeoLite2-City-Blocks-IPv4.csv";
 // https://www.npmjs.com/package/csv-parser
 // used a library to save time. 
 try {
-    if(!fs.existsSync("./points.geojson")) {
+    if (!fs.existsSync("./points.geojson")) {
         fs.createReadStream(file)
-        .pipe(parse())
-        .on('data', data => {
-            //I like to build it into a temporary variable
-            let temp = {
-                "type": "Feature",
-                "properties": {
-                    "dbh": 1
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [data.longitude, data.latitude]
+            .pipe(parse())
+            .on('data', data => {
+                //I like to build it into a temporary variable
+                let temp = {
+                    "type": "Feature",
+                    "properties": {
+                        "dbh": 1
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [data.longitude, data.latitude]
+                    }
                 }
-            }
-            results[0]["features"].push(temp)
-        })
-        .on('end', () => {
-            fs.writeFileSync("./points.geojson", JSON.stringify(results));
-        })
-    } 
+                results.push(temp)
+            })
+            .on('end', () => {
+                fs.writeFileSync("./points.geojson", JSON.stringify(results));
+            })
+    }
     else {
         console.log("File already exists and does not need to be generated.");
     }
-} catch(err) {
+} catch (err) {
     console.error(err);
 }
 
 
 const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname + "/public")))
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 })
-app.get('/request?top=&bot=')
-// app.get('/', function(req, res) {
-//     res.sendFile("./trees.geojson");
-// })
+app.get('/request?top=&btm=', (req, res) => {
+    if (!req.query.top || !req.query.btm) {
+        console.error("No query sent");
+    }
+    else {
+        console.log(req.query.topLng, req.query.topLat, req.query.btmLng, req.query.btmLat);
+        res.send(getPoints(req.query.topLng, req.query.topLat, req.query.btmLng, req.query.btmLat));
+    }
+})
 app.listen(PORT);
 
+
+function getPoints(topLng, topLat, btmLng, btmLat) {
+    var arr = [{
+        "type": "FeatureCollection",
+        "features": [],
+    }];
+    arr[0]["features"] = results.filter(
+        point => {
+            point[0] <= topLng &&
+                point[0] >= btmLng &&
+                point[1] <= topLat &&
+                point[1] >= btmLat
+        }
+    );
+    console.log(arr);
+    return arr;
+}
 
 
 
